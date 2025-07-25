a65db4564c2eb377d400d791e207394f5ec4909f
ivan.gutz <ivan.gutz@alunos.sc.senac.br>
2025-07-25 19:36:36 -0300

    chore update code

--- app/agents/models.py
+++ app/agents/models.py
@@ -9,12 +9,12 @@ gemini_api_key = os.getenv("GEMINI_API_KEY")
 llm_gemini_flash = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     api_key=gemini_api_key,
-    temperature=1,
+    temperature=0.6,
 )
 
 llm_gemini_pro = ChatGoogleGenerativeAI(
     model="gemini-2.5-pro",
     api_key=gemini_api_key,
-    temperature=1,
+    temperature=0.6,
 )
 
